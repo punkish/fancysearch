df9b95e6bafd93ea68c74d5e2383430859fa2dd3
punkish <punk.kish@gmail.com>
2018-02-03 21:46:37 +0100

    fancySearch now supports ajax calls and mulitple instances on the same page

--- js/fancysearch.js
+++ js/fancysearch.js
@@ -5,12 +5,16 @@
     License: Released into the public domain under CC0
 */
 
-const fancySearch = (function() {
+"use strict";
+
+const fancySearch = function() {
 
     // fsDiv is the container in which fancySearch is created
     // Everything happens inside fsDiv. It is set during init()
     let fsDiv;
+    let facets;
     let facetKeys;
+    let facetValues;
 
 	const makeFsContainer = function() {
 
@@ -69,7 +73,7 @@ const fancySearch = (function() {
     
         event = event || window.event;
         const keyPressed = event.which || event.keyCode;
-    
+
         let thisInput = event.target;
         let fsp = thisInput.parentElement;
         let fsk = fsp.children[0];
@@ -80,7 +84,7 @@ const fancySearch = (function() {
         
         if (keyPressed == ENTER) {
             
-            if (fsk.innerText === 'TEXT:' && fsvInput.value !== '') {
+            if (fsvInput.value !== '') {
                 fsp.className = 'fs-param-filled';
                 fsv.innerText = fsvInput.value;
                 fsv.className = 'fs-val on';
@@ -95,18 +99,34 @@ const fancySearch = (function() {
             return false;
         }
         else if (keyPressed == BACKSPACE || keyPressed == DEL) {
-    
+            console.log('a');
             if (thisInput.value === '') {
+                console.log('b');
 
                 let allFsp = document.querySelectorAll(`div#${fsDiv.id} div.fs-param-filled`);
                 let lastFsp = allFsp[allFsp.length - 1];
     
-                if (lastFsp.className === 'fs-param-filled') {
-                    lastFsp.className = 'fs-param-filled hilite';
+                if (lastFsp) {
+                    console.log('h');
+                    if (lastFsp.className === 'fs-param-filled') {
+                        lastFsp.className = 'fs-param-filled hilite';
+                    }
+                    else if (lastFsp.className = 'fs-param-filled hilite') {
+                        lastFsp.parentElement.removeChild(lastFsp);
+                    }
                 }
-                else if (lastFsp.className = 'fs-param-filled hilite') {
+                else {
+                    console.log('i');
+
+                    let allFsp = document.querySelectorAll(`div#${fsDiv.id} div.fs-param-empty`);
+                    let lastFsp = allFsp[allFsp.length - 1];
+
                     lastFsp.parentElement.removeChild(lastFsp);
                 }
+                
+            }
+            else {
+                console.log('c');
             }
         }
     };
@@ -120,20 +140,26 @@ const fancySearch = (function() {
     
         new autoComplete({
             selector: selector,
-            minChars: 0,
-            source: function(term, suggest) {
+            minChars: Array.isArray(choices) ? 0 : 3,
+            source: function(term, response) {
     
-                term = term.toLowerCase();
-                let matches = [];
-                let j = choices.length;
-    
-                for (let i = 0; i < j; i++) {
-                    if (~choices[i].toLowerCase().indexOf(term)) {
-                        matches.push(choices[i]);
+                if (Array.isArray(choices)) {
+                    term = term.toLowerCase();
+                    let matches = [];
+                    let j = choices.length;
+        
+                    for (let i = 0; i < j; i++) {
+                        if (~choices[i].toLowerCase().indexOf(term)) {
+                            matches.push(choices[i]);
+                        }
                     }
+        
+                    response(matches);
                 }
-    
-                suggest(matches);
+                else {
+                    try { xhr.abort(); } catch(e){}
+                    xhr(choices + '/' + term, response);
+                }
             },
             onSelect: function(e, term, item) {
     
@@ -195,11 +221,32 @@ const fancySearch = (function() {
         });
     };
 
+    const xhr = function(href, cb) {
+        let x = new XMLHttpRequest();
+
+        x.onload = function(event) {
+            if (x.readyState === 4) {
+                if (x.status === 200) {
+                    const res = JSON.parse(x.responseText);
+                    cb(res);
+                }
+            }
+        };
+
+        x.onerror = function(e) {
+            console.error(x.statusText);
+        };
+
+        x.open("GET", href, true);
+        x.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        x.send();
+    };
+
 	return  {
         
         result: function() {
         
-            const params = fs.childNodes;
+            const params = fsDiv.childNodes;
             let q = {};
             for (let i = 0, j = params.length; i < j; i++) {
                 const kv = params[i].children;
@@ -227,13 +274,15 @@ const fancySearch = (function() {
             return q;
         },
 
-		init: function(selector, facets) {
+		init: function(selector, data) {
             
             // https://stackoverflow.com/questions/7028145/find-key-name-in-hash-with-only-one-key
-            facetKeys = facets.map(element => { return element['key'] });
             fsDiv = selector;
+            facets = data;
+            facetKeys = facets.map(element => { return element['key'] });
+            facetValues = facets.map(element => { return element['values'] });
             
             makeFsContainer();
 		}
 	};
-})();
+};
